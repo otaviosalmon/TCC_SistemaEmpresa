19a12ae2a46063e49f6cbe992a18165d4f14849d
Otavio <otaviosal@hotmail.com>
2026-02-20 19:33:50 -0300

    projeto recriado

---
+++ TCC_SistemaEmpresa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TCC_SistemaEmpresa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
