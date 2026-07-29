5304610df2365ac8e9ba88b1ca77bbfc9972350d
Otavio <otaviosal@hotmail.com>
2026-07-29 12:48:42 -0300

    alterações em appdbcontext

--- TCC_SistemaEmpresa/obj/Debug/net8.0/TCC_SistemaEmpresa.AssemblyInfo.cs
+++ TCC_SistemaEmpresa/obj/Debug/net8.0/TCC_SistemaEmpresa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCC_SistemaEmpresa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64b7ad3b842d0b83c020007579023d5c5a703cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe36fe70c87db07ff052d1139fb2fac83e9a751")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCC_SistemaEmpresa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCC_SistemaEmpresa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
